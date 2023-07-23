7de7ec3df01a5273ef5e5a34e282e17343e51ff0
clf94-dev <clf94@hotmail.es>
2023-07-23 08:06:00 +0200

    Initial Commit

---
+++ JustWalkingApp/JustWalkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWalkingAppApp.swift
+//  JustWalkingApp
+//
+//  Created by Carmen Lucas on 23/7/23.
+//
+
+import SwiftUI
+
+@main
+struct JustWalkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
